291f602bdb28312072058cb6baf99c7abcddbe66
fabiopacifici <boolean@fabiopacifici.com>
2026-02-11 10:38:07 +0100

    complete snack 4

---
+++ snack-funzioni/04_conta_vocali/script.js
@@ -0,0 +1,38 @@
+/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
+
+const word = 'javascript';
+
+
+// Dichiara la funzione qui.
+function countVowels(str){
+  
+  // store the counter for every vowel found
+  let counter = 0
+  let foundVowels = []
+  const vowels = ['a','e','i','o','u']
+
+  for(let i = 0; i < str.length; i++){
+    const char = str[i]
+    console.log(char);
+    if(vowels.includes(char)){
+      foundVowels.push(char)
+      counter++
+    }
+    
+    
+  }
+
+  //console.log(counter, foundVowels, foundVowels.length);
+  
+  // return the counter
+  return foundVowels.length
+
+}
+
+
+// Invoca la funzione qui e stampa il risultato in console
+const foundVowels = countVowels(word)
+
+
+//Risultato atteso se si passa 'javascript': 3 (a, a, i)
+console.log(foundVowels);
